95983e25e2c6693c8bdb1af33a13f1b7eb254c82
seitamuro <seitamuro0210@gmail.com>
2021-08-17 11:31:35 +0900

    solve p020

---
+++ p020/src/main.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: u64,
+        b: u32,
+        c: u64,
+    }
+
+    if a < c.pow(b) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
